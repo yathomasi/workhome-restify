ea584010cf9ee0abf311370b8c9d4b138448f22b
Thomas Kunwar <yathomasi@gmail.com>
2019-12-29 20:12:53 +0545

    added required config

--- config/config.js
+++ config/config.js
@@ -4,17 +4,19 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 module.exports = {
-  NAME:"restify-api",
+  NAME: "restify-api",
   ENV: process.env.NODE_ENV || "development",
   PORT: process.env.PORT || 3000,
   URL: process.env.BASE_URL || "http://localhost:3000",
   JWT_SECRET: process.env.JWT_SECRET || "secretlysecreteverjwttokengenerate",
   DB: {
     get: mysql.createConnection({
-      host: process.env.DB_HOST ,
-      user: process.env.DB_USER ,
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
       password: process.env.DB_PASS,
-      database: process.env.DB_NAME 
+      database: process.env.DB_NAME,
+      multipleStatements: true
     })
-  }
+  },
+  SALT: process.env.SALT || 14
 };
